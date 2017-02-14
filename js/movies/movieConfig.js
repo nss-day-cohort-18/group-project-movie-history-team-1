a5bd1b473ef03bdbbfc73f31d4a2c6115d3ffbdb
Adam Myers <Tavern@Adams-Mac.local>
2017-02-14 09:25:40 -0600

    movie Config file

--- js/movies/movieConfig.js
+++ js/movies/movieConfig.js
@@ -1,1 +1,11 @@
 "use strict";
+
+var movieConfig = {
+  url: "http://www.omdbapi.com/?"
+};
+
+function getMovieURL(){
+	return movieConfig;
+}
+
+module.exports = {getMovieURL}
--- js/movies/movieLoad.js
+++ js/movies/movieLoad.js
@@ -1,11 +1,12 @@
 "use strict";
 
+let movieConfig = require("./movieConfig.js");
 
 function pullMovie(searchTitle) {
 
 return new Promise( function(resolve, reject){
 		$.ajax({
-	    url: "http://www.omdbapi.com/?",
+	    url: movieConfig.getMovieURL().url,
 	    type: 'GET',
 	    data: { t: searchTitle, tomatoes: true},
 	    success: resolve()
